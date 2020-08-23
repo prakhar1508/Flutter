dd6619f9cb1abb927d3b1b87338b3527ebc376b6
prakhar1508 <prakhar.manakbohara@gmail.com>
2020-08-24 01:12:49 +0530

    oop class

--- Day18/oops.dart
+++ Day18/oops.dart
@@ -1,1 +1,12 @@
+import 'mycentral.dart';
 
+void main() {
+  // instantiation
+  var visitor1 = VisitorForm(name: "Prakhar", mobile: 998383, gender: "Male");
+  // var visitor2 = VisitorForm("Smith", "Male", 88442);
+  VisitorForm visitor3 = VisitorForm(name: "Emma", gender: "Female");
+  print(visitor1.name);
+  print(visitor1.gender);
+  print(visitor3.mobile);
+  print(visitor3.name);
+}
